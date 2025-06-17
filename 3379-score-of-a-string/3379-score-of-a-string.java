9d5b4db52b87ae8b8582113a26d9adb6d4d4e394
DEVARASAIGANESH29 <119840486+DEVARASAIGANESH29@users.noreply.github.com>
2025-06-17 08:58:28 +0530

    Time: 1 ms (99.59%), Space: 42.3 MB (15.54%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.java
+++ 3379-score-of-a-string/3379-score-of-a-string.java
@@ -1,14 +1,13 @@
 class Solution {
     public int scoreOfString(String s) {
-        int asci[]=new int[s.length()];
-        int sum=0;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            asci[i]=(int)ch;
+        int arr[] = new int [s.length()];
+        for(int i = 0; i < s.length(); i++){
+           char ch = s.charAt(i);
+            arr[i] = (int) ch;
         }
-        for(int i=1;i<s.length();i++)
-        {
-            sum+=Math.abs(asci[i]-asci[i-1]);
+        int sum = 0;
+        for(int i = 1; i<s.length(); i++){
+            sum += Math.abs(arr[i] - arr[i-1]);
         }
         return sum;
     }
