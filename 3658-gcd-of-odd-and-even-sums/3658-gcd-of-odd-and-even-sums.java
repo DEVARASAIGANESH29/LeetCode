21e5f9543bf3c4611283fe0517d7bd265d461c40
DEVARASAIGANESH29 <119840486+DEVARASAIGANESH29@users.noreply.github.com>
2026-07-15 08:34:59 +0530

    Time: 1 ms (83.11%), Space: 42.8 MB (6.12%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,21 +1,7 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int oddCount = 0;
-        int evenCount = 0;
-        for(int i=0;i <= n*2;i++){
-            if(i%2 == 0){
-                evenCount += i;
-            }
-            else{
-                oddCount += i;
-            }
-        }
-        while(evenCount != 0){
-            int temp = evenCount;
-            evenCount = oddCount % evenCount;
-            oddCount = temp;
-        }
-        return oddCount;
-        
+        int odd = n*n;
+        int even = n*(n+1);
+        return n;
     }
 }
